48db6c23b99205e425d036ed35ffb785e6f746d0
Anar Seferov <1anarseferov@gmail.com>
2020-12-13 13:34:23 +0400

    Updates

---
+++ precache-manifest.276c461255c02c853ca543a4ce1aa1ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cadc514461d7b5f6f790f0ede4a34205",
+    "url": "/filterapp/index.html"
+  },
+  {
+    "revision": "eaa1a8d020a53bcea532",
+    "url": "/filterapp/static/css/main.9b300132.chunk.css"
+  },
+  {
+    "revision": "e9aa10e17ed53eeec77e",
+    "url": "/filterapp/static/js/2.2cad2a06.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/filterapp/static/js/2.2cad2a06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaa1a8d020a53bcea532",
+    "url": "/filterapp/static/js/main.ff8c65df.chunk.js"
+  },
+  {
+    "revision": "92c17947c76ee3fc1a92",
+    "url": "/filterapp/static/js/runtime-main.cc5c97a7.js"
+  }
+]);
